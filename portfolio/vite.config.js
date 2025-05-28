d6c3a19e711839849d661c9550b800647edf26b8
Pranith Konda <pranithkonda@Praniths-MacBook-Air.local>
2025-05-28 20:34:25 +0530

    updated

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: ".", // Ensures Vite picks the project root
   plugins: [react()],
 });
